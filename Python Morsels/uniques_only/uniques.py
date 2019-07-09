e1ccd3b4d1f2397c775ce335b5ec08945256bd46
keithandrew <keithandrewosullivan@gmail.com>
2019-07-09 17:57:33 +0100

    Base + Challenge 2 complete

--- Python Morsels/uniques_only/uniques.py
+++ Python Morsels/uniques_only/uniques.py
@@ -6,8 +6,3 @@ def uniques_only(iterator):
             uniques.append(item)
 
     print(uniques)
-
-
-hashables = [(n, n + 1) for n in range(1000)] + [0]
-unhashables = [[n] for n in range(1000)] + [0]
-uniques_only(unhashables)
