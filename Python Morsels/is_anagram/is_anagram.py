e72d9cace4e45925cc4b04f6214965d928556c77
keithandrew <keithandrewosullivan@gmail.com>
2019-07-16 18:33:46 +0100

    Challenge 3 complete

--- Python Morsels/is_anagram/is_anagram.py
+++ Python Morsels/is_anagram/is_anagram.py
@@ -1,7 +1,8 @@
 def is_anagram(string1, string2):
-    comp_1 = sorted(string1.replace(" ", "").lower())
-    comp_2 = sorted(string2.replace(" ", "").lower())
-    return comp_1 == comp_2
+    comp_1 = [letter for letter in string1.lower() if letter.isalpha()]
+    comp_2 = [letter for letter in string2.lower() if letter.isalpha()]
+
+    print(sorted(comp_1) == sorted(comp_2))
 
 
-is_anagram("Coins kept", "in pockets")
+is_anagram("a diet", "I'd eat")
