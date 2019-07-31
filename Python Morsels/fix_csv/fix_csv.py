25488591b1633bf298be43b61cf524c89c44f8d5
keithandrew <keithandrewosullivan@gmail.com>
2019-07-31 20:27:42 +0100

    .

--- Python Morsels/fix_csv/fix_csv.py
+++ Python Morsels/fix_csv/fix_csv.py
@@ -21,13 +21,14 @@ with open(args.source) as csvfile:
     if args.in_quote:
         arguments["quotechar"] = args.in_quote
 
+    # if no delimiter or quote specified, then sniff the file and return dialect
     if not args.in_delimiter and not args.in_quote:
         arguments["dialect"] = csv.Sniffer().sniff(csvfile.read())
         csvfile.seek(0)
 
     reader = csv.reader(csvfile, **arguments)
 
-    # write new csvfile
+    # write new csv file
     with open(args.output, "w", newline="") as csv_output:
         writer = csv.writer(csv_output)
         writer.writerows(row for row in reader)
