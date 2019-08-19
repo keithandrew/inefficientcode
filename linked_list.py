3b1d072a160281c756112fe97cddbf776d950da2
keithandrew <keithandrewosullivan@gmail.com>
2019-08-19 02:20:10 +0100

    linked list class next & previous node implemented list constructor implemented search nth node implemented last node implemented reset to head_node implemented list_print implemented list_length implemented

--- linked_list.py
+++ linked_list.py
@@ -4,9 +4,7 @@ class Node:
     def __init__(self, data=0):
         self.data = data
         self.next = None
-
-    def __str__(self):
-        return str(self.data)
+        self.previous = None
 
     def __repr__(self):
         return str(self.data)
@@ -17,18 +15,43 @@ class LinkedList:
 
     def __init__(self):
         self.head_value = None
+        self.current_node = None
+
+    def add_node(self, value):
+        if not self.head_value:
+            self.head_value = Node(value)
+            self.current_node = self.head_value
+            self.current_node.previous = None
+        else:
+            new_node = Node(value)
+            previous = self.current_node
+            self.current_node.next = new_node
+            self.current_node = new_node
+            self.current_node.previous = previous
 
     def build(self, array):
-        array = array
-        self.head_value = array[0]
-        current_node = Node(self.head_value)
+        for element in array:
+            self.add_node(element)
+        self.reset()
 
-        for key, value in enumerate(array[1::], 1):
-            current_node.next = Node(value)
-            current_node = current_node.next
-            print(current_node)
+    def reset(self):
+        self.current_node = self.head_value
 
-    def listPrint(self):
+    def next(self):
+        if self.current_node:
+            self.current_node = self.current_node.next
+            return self.current_node
+        else:
+            return None
+
+    def previous(self):
+        if self.current_node:
+            self.current_node = self.current_node.previous
+            return self.current_node
+        else:
+            return None
+
+    def list_print(self):
         node_list = []
         current_node = self.head_value
 
@@ -36,9 +59,9 @@ class LinkedList:
             node_list.append(current_node)
             current_node = current_node.next
 
-        return node_list
+        print(node_list)
 
-    def listLength(self):
+    def list_length(self):
         listLength = 0
         current_node = self.head_value
 
@@ -48,7 +71,26 @@ class LinkedList:
 
         return listLength
 
+    def locate(self, n):
+        index = 1
+        self.reset()
+        while index < n:
+            self.next()
+            index += 1
+        return self.current_node
+
+    def last_node(self):
+        while self.current_node.next:
+            self.next()
+        return self.current_node
+
 
 testList = LinkedList()
-testList.build([1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(testList.listPrint())
+testList.build([2, 4, 6, 8, 10, 12, 14, 16, 18])
+print(testList.list_print())
+print(testList.current_node)
+print(testList.current_node.next)
+print(testList.last_node())
+print(testList.current_node)
+print(testList.locate(5))
+print(testList.current_node)
